b5cc315a9b501ff0727667dad0bd2af4e53b861b
Jamie <pomprab.b@gmail.com>
2026-07-27 18:17:53 +0200

    added newline for matrix display

--- ex10.py
+++ ex10.py
@@ -6,7 +6,7 @@ from matrix import Matrix
 
 def main():
     '''Test case for exercise 10'''
-    print("Results for exercise 10")
+    print("Results for exercise 10\n")
 
     Matrix([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]]).row_echelon().print_matrix()
     # identity, unchanged
@@ -26,13 +26,13 @@ def main():
     # [0.0, 0.0, 1.0, 0.0, -3.6666667]
     # [0.0, 0.0, 0.0, 1.0, 29.5]
 
-    print("-- eval sheet extra cases --")
+    print("-- eval sheet extra cases --\n")
     Matrix([[0., 0.], [0., 0.]]).row_echelon().print_matrix()  # [0, 0] [0, 0]
     Matrix([[4., 2.], [2., 1.]]).row_echelon().print_matrix()  # [1, 0.5] [0, 0]
     Matrix([[-7., 2.], [4., 8.]]).row_echelon().print_matrix()  # [1, 0] [0, 1]
     Matrix([[1., 2.], [4., 8.]]).row_echelon().print_matrix()  # [1, 2] [0, 0]
 
-    print("-- K = Complex --")
+    print("-- K = Complex --\n")
     i = Complex(0., 1.)
     Matrix([[Complex(1.), Complex(0.)], [Complex(0.), i]]).row_echelon().print_matrix()
     Matrix([[Complex(2.), Complex(0., 1.)], [Complex(4.), Complex(0., 2.)]]) \
